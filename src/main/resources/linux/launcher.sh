8b3def131fe306e48b54a105d0ebf0992e85cbab
matt <unknown>
2009-06-19 20:11:49 +0000

    Experimental first cut at a script, and latest m2eclipse config

---
+++ src/main/resources/linux/launcher.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# resolve symlinks to launch script
+#
+
+PRG=$0
+
+while [ -h "$PRG" ]; do
+    ls=`ls -ld "$PRG"`
+    link=`expr "$ls" : '^.*-> \(.*\)$' 2>/dev/null`
+    if expr "$link" : '^/' 2> /dev/null >/dev/null; then
+	PRG="$link"
+    else
+	PRG="`dirname "$PRG"`/$link"
+    fi
+done
+
+progdir=`dirname "$PRG"`
+APPNAME=`basename "$0"`
+
+sh=sh
+# #73162: Ubuntu uses the ancient Bourne shell, which does not implement trap well.
+if [ -x /bin/bash ]
+then
+	sh=/bin/bash
+fi
+eval exec $sh java -cp $progdir/lib ${xplp.mainclassname}
+exit 1
